45e0e01407eece9fd8b87fc9c9a99f3f597c8525
Udit Bansal <Udit.ban2312@gmail.com>
2025-07-14 23:31:14 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const indexRouter = require('./routes/index');
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+app.listen(3000);
