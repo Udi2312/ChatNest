eca7aba1e203e66901f0b56636152cfe5f467df8
Udit Bansal <Udit.ban2312@gmail.com>
2025-07-17 00:03:41 +0530

    commit

--- app.js
+++ app.js
@@ -2,6 +2,29 @@ const express = require('express');
 const app = express();
 const indexRouter = require('./routes/index');
 const path = require('path');
+const socketIO = require('socket.io');
+
+const http = require('http');
+const server = http.createServer(app);
+const io = socketIO(server)
+
+let waitingusers = [];
+let rooms = {};
+
+io.on('connection', function(socket){
+   socket.on('joinroom', function(){
+   if(waitingusers.length>0){
+    let partner = waitingusers.shift();
+    const roomname= `${socket.id}-${partner.id}`
+    socket.join(roomname);
+    partner.join(roomname);
+    io.to(roomname).emit('joined')
+   }
+   else{
+    waitingusers.push(socket);
+   }
+   })
+})
 
 app.set('view engine', 'ejs');
 app.use(express.json());
@@ -10,4 +33,4 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
-app.listen(3000);
+server.listen(3000);
