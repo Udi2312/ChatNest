4137a3c8edf072d012e445f00d2d6667b9e024e3
Udit Bansal <Udit.ban2312@gmail.com>
2025-07-16 19:26:48 +0530

    commit

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,8 @@ const router = express.Router();
 router.get('/', (req, res) => {
     res.render('index')
 })
+router.get('/chat', (req, res) => {
+    res.render('chat')
+})
 
 module.exports = router;
